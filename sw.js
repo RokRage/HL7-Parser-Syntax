2949b54cf42a63876f13dc912079aa1fa277ef06
RokRage <jfy6d2zq29@privaterelay.appleid.com>
2026-07-16 18:44:01 +0100

    ui fix

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_VERSION = "hl7-message-explorer-v17";
+const CACHE_VERSION = "hl7-message-explorer-v18";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./assets/css/styles.css?v=callout-neon-2",
+  "./assets/css/styles.css?v=callout-neon-3",
   "./assets/js/app.js?v=subfield-highlight-2",
   "./assets/js/hl7-fields-2x.js",
   "./assets/js/hl7-datatypes-2x.js",
